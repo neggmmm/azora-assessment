26d3043269e9d96695777b0423ae37089eafb7d9
neggmmm <kekkdead@gmail.com>
2026-01-14 18:22:54 +0200

    fixing grid in larger screens and add hover animations

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,20 +2,21 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div className='h-[160vh] mx-40 my-20'>
+    <div className='h-[160vh] mx-auto max-w-[1200px] px-10 my-20'>
+
         <div className='flex justify-between items-center my-20'>
             <h1 className='w-[420px] text-4xl'>What you get on Setapp.</h1>
             <p className='w-[460px] font-light'>With a single monthly subscription at $9.99, you get 240+ apps for your Mac.</p>
         </div>
         <div className='grid grid-cols-2 gap-6 my-20'>
-            <img className='col-span-2' src='./about/Link.png'></img>
-            <img src='./about/Link (1).png'></img>
-            <img src='./about/Link (2).png'></img>
+            <img className='col-span-2 hover:scale-105 transition-all' src='./about/Link.png'></img>
+            <img className='hover:scale-105 transition-all' src='./about/Link (1).png '></img>
+            <img className='hover:scale-105 transition-all' src='./about/Link (2).png'></img>
         </div>
 
-        <div className='flex justify-center gap-2'>
-            <img className='' src='./about/Vector (1).png'></img>
-            <p>View all superpowers</p>
+        <div className=' group flex justify-center gap-2 cursor-pointer'>
+            <img className='transition-transform duration-300 group-hover:-translate-x-2' src='./about/Vector (1).png'></img>
+            <p className='transition-transform duration-300 group-hover:translate-x-2'>View all superpowers</p>
         </div>
 
         <div className='flex justify-between items-center my-20'>
