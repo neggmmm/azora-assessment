6a37f8902d4947d90f2b0f2d7d7a49831bae0d36
neggmmm <kekkdead@gmail.com>
2026-01-14 13:59:52 +0200

    finishing setapp journey section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Divider from './components/Divider'
 import Navbar from './components/navbar'
+import About from './pages/About'
 import Hero from './pages/Hero'
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
     <>
     <Navbar />
      <Hero />
+     <Divider />
+     <About />
      <Divider />
     </>
   )
---
+++ src/pages/About.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='mx-40 my-20'>
+        <div className='flex justify-between items-center my-20'>
+            <h1 className='w-[420px] text-4xl'>What you get on Setapp.</h1>
+            <p className='w-[460px] font-light'>With a single monthly subscription at $9.99, you get 240+ apps for your Mac.</p>
+        </div>
+        <div className='grid grid-cols-2 gap-6 my-20'>
+            <img className='col-span-2' src='./about/Link.png'></img>
+            <img src='./about/Link (1).png'></img>
+            <img src='./about/Link (2).png'></img>
+        </div>
+
+        <div className='flex justify-center gap-2'>
+            <img className='' src='./about/Vector (1).png'></img>
+            <p>View all superpowers</p>
+        </div>
+
+        <div className='flex justify-between items-center my-20'>
+            <h1 className='w-[420px] text-4xl'>Your Setapp journey</h1>
+            <p className='w-[460px] font-light'>Type in your task into Setapp search and get instant app recommendations.</p>
+        </div>
+    </div>
+  )
+}
